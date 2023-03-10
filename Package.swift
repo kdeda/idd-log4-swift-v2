24f19e8668490a44ac3ca37b63980e770f7eb2d6
Klajd Deda <kdeda@id-design.com>
2023-03-09 21:42:50 -0500

    Linux

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "idd-log4-swift",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v10_13)
+        .iOS(.v15),
+        .macOS(.v11)
     ],
     products: [
         .library(
